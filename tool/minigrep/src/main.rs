de90a961df714ba6cf1ab1a7375b274763b60a93
Lance She <15201752137@163.com>
2019-03-25 20:53:41 +0800

    Add error handling

--- tool/minigrep/src/main.rs
+++ tool/minigrep/src/main.rs
@@ -4,14 +4,30 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
     
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
